8d1fee0dfa772ef9ca41c263b7cc4a29ff0edb91
Serega Schugani <Schuganiserega@mail.ru>
2022-06-22 16:01:09 +0400

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Data;
+
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products => Set<Product>();
+
+    public AppDbContext(
+        DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+    }
+}
